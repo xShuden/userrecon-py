1e949193ee0d6f6c5c941583efcc95f5853dafc3
decoxviii <decoxviii@gmail.com>
2019-06-16 14:58:06 -0500

    add web_accounts_list.json

--- userrecon/core.py
+++ userrecon/core.py
@@ -8,58 +8,82 @@ import sys
 
 try:
     from userrecon.version import __version__, __author__
-    from userrecon.social_networks import links
-
     import aiohttp
+
 except ModuleNotFoundError as error:
-    print('[-] error: {}'.format(error))
+    print("[-] error: {}".format(error))
     sys.exit(1)
 
+
 def print_sexy_banner():
-    print("""                                                                    
+    print(
+        """                                                                    
   __  __________  _____________  _________  ____        ____  __  __
  / / / / ___/ _ \\/ ___/ ___/ _ \\/ ___/ __ \\/ __ \\______/ __ \\/ / / /
 / /_/ (__  )  __/ /  / /  /  __/ /__/ /_/ / / / /_____/ /_/ / /_/ / 
 \\__,_/____/\\___/_/  /_/   \\___/\\___/\\____/_/ /_/     / .___/\\__, /  
        {}Version: {} | Author: {}{}           /_/    /____/   
-""".format('\033[32m', __version__, __author__, '\033[0m'))
+""".format(
+            "\033[32m", __version__, __author__, "\033[0m"
+        )
+    )
 
-async def check_status(session, social_network, url, username):
-    
+
+async def check_status(session, social_network, username):
+
     global results
     results = {}
 
-    url = url.format(username)
+    account_missing_string = social_network["account_missing_string"]
+    url = social_network["check_uri"].format(account=username)
+    name = social_network["name"]
+
     try:
         async with session.get(url) as resp:
-            print('\033[33m[!] {:10}: {}\033[0m\033[J'.format(social_network, url), end='\r')
-            if resp.status == 200:
-                print('\033[32m[+] {:10}: {}\033[0m\033[J'.format(social_network, url))
-                results[social_network] = url
+            print("\033[33m[!] {:10}: {}\033[0m\033[J".format(name, url), end="\r")
+
+            text = await resp.text()
+            if resp.status == 200 and not account_missing_string in text:
+                print(
+                    "\033[32m[+] {} : {}\033[0m\033[J".format(
+                        social_network["name"], url
+                    )
+                )
+                results[social_network["name"]] = url
+
         return resp.release()
     except:
         pass
 
+
 async def verify_username(username):
-    
-    print('[*] checking username {}{}{} in {} social networks'.format(
-        '\033[1m', username, '\033[0m', len(links.keys())
-    ))
+
+    with open("userrecon/web_accounts_list.json", "r") as data_file:
+        web_accounts_list = json.load(data_file)
+        social_networks = web_accounts_list["sites"]
+
+    print(
+        "[*] checking username {}{}{} in {} social networks".format(
+            "\033[1m", username, "\033[0m", len(social_networks)
+        )
+    )
     start = time()
     async with aiohttp.ClientSession() as session:
         tasks = [
-            check_status(session, social_network, url, username)
-            for social_network, url in links.items() 
+            check_status(session, social_network, username)
+            for social_network in social_networks
         ]
         await asyncio.gather(*tasks)
-        print('[*] {}{}{} results found in: {:.2f} segs.\033[J'.format(
-            '\033[1m', len(results.keys()), '\033[0m', time()-start
-        ))
+        print(
+            "[*] {}{}{} results found in: {:.2f} segs.\033[J".format(
+                "\033[1m", len(results.keys()), "\033[0m", time() - start
+            )
+        )
+
 
 def generate_json_file(filename):
 
     output = json.dumps(results, indent=2)
-    with open('{}.json'.format(filename), 'w') as f:
+    with open("{}.json".format(filename), "w") as f:
         f.write(output)
-        print('[*] file {} was created'.format(filename))
-
+        print("[*] file {} was created".format(filename))
